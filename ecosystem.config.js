7d5c28ed8c6b232d6ff80538a946a5fbf784d8a3
Nelie Taylor <nghi@huongda.com>
2021-10-31 23:42:19 +0700

    Complete auto deploy dev

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,16 +1,18 @@
 module.exports = {
-  apps: [{
-    name: '5Me Dev',
-    script: 'dist/server.js',
-    exec_mode: 'cluster',
-    instances: 1,
-    autorestart: true,
-    watch: false,
-    ignore_watch: ['node_modules', 'logs'],
-    max_memory_restart: '1G',
-    env: {
-      PORT: 2299,
-      NODE_ENV: 'production',
+  apps: [
+    {
+      name: '5Me Dev',
+      script: 'dist/server.js',
+      exec_mode: 'cluster',
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      ignore_watch: ['node_modules', 'logs'],
+      max_memory_restart: '1G',
+      env: {
+        PORT: 2299,
+        NODE_ENV: 'production',
+      },
     },
-  }]
+  ],
 };
